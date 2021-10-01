8a40ec614aef63becd0d56718a55b3f3495dc2b1
deimantas Jakovlevas <deimantas@zappar.com>
2021-10-01 02:17:32 +0100

    simplify promises

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 import axios from "axios";
 
-export const latest = async (): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get("https://prices.runescape.wiki/api/v1/osrs/latest")
-      .then((res) => resolve(res.data.data))
-      .catch((err) => reject(err));
-  });
-};
+export const latest = async (): Promise<string> => (await axios.get("https://prices.runescape.wiki/api/v1/osrs/latest")).data.data;
 
-export const id = async (id: number): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`https://prices.runescape.wiki/api/v1/osrs/latest?id=${id}`)
-      .then((res) => resolve(res.data.data))
-      .catch((err) => reject(err));
-  });
-};
+export const id = async (id: number): Promise<string> => (await axios.get(`https://prices.runescape.wiki/api/v1/osrs/latest?id=${id}`)).data.data;
